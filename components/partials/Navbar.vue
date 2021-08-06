bea9edac9af68d1ddda8e0d87ab13f654e063fce
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-08-06 22:45:37 +0100

    fix some item of menu (navbar)

--- components/partials/Navbar.vue
+++ components/partials/Navbar.vue
@@ -1,10 +1,10 @@
 <template>
     <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
         <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-            <a class="navbar-brand brand-logo" href="index.html"><img src="images/logo.svg" alt="logo"/></a>
+            <a class="navbar-brand brand-logo" href="index.html"><strong>One Touch</strong></a>
             <a class="navbar-brand brand-logo-mini" href="index.html"><img src="images/logo-mini.svg" alt="logo"/></a>
             <button class="navbar-toggler navbar-toggler align-self-center d-none d-lg-flex" type="button" data-toggle="minimize">
-            <span class="typcn typcn-th-menu"></span>
+                <span class="typcn typcn-th-menu"></span>
             </button>
         </div>
         <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
@@ -59,18 +59,18 @@
                 </li>
                 <li class="nav-item nav-profile dropdown">
                     <a class="nav-link dropdown-toggle  pl-0 pr-0" href="#" data-toggle="dropdown" id="profileDropdown">
-                    <i class="typcn typcn-user-outline mr-0"></i>
-                    <span class="nav-profile-name">Evan Morales</span>
+                        <i class="typcn typcn-user-outline mr-0"></i>
+                        <span class="nav-profile-name">Evan Morales</span>
                     </a>
                     <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
-                    <a class="dropdown-item">
-                    <i class="typcn typcn-cog text-primary"></i>
-                    Settings
-                    </a>
-                    <a class="dropdown-item">
-                    <i class="typcn typcn-power text-primary"></i>
-                    Logout
-                    </a>
+                        <a class="dropdown-item">
+                            <i class="typcn typcn-user text-primary"></i>
+                            Profile
+                        </a>
+                        <a class="dropdown-item">
+                            <i class="typcn typcn-power text-primary"></i>
+                            Déconnexion
+                        </a>
                     </div>
                 </li>
             </ul>
