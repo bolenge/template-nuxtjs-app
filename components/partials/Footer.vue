72000e5a74d9cb89338e0740585b9839afc91f66
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-08-06 22:45:16 +0100

    fix add copyright and powered by Mtech interactives

--- components/partials/Footer.vue
+++ components/partials/Footer.vue
@@ -1,18 +1,26 @@
 <template>
   <footer class="footer">
     <div class="d-sm-flex justify-content-center justify-content-sm-between">
-      <span class="text-center text-sm-left d-block d-sm-inline-block">Copyright © <a href="https://www.bootstrapdash.com/" target="_blank">bootstrapdash.com</a> 2020</span>
-      <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Free <a href="https://www.bootstrapdash.com/" target="_blank">Bootstrap dashboard </a>templates from Bootstrapdash.com</span>
+      <span class="text-center text-sm-left d-block d-sm-inline-block">Copyright © <a href="#" target="_blank" class="text-blue text-bold">One Touch</a> {{ year }} </span>
+      <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Powered by <a href="https://mtechinteractives.com/" target="_blank" class="text-blue text-bold">Mtech Interactives</a></span>
     </div>
   </footer>
 </template>
 
 <script>
 export default {
-  components: {
+  computed: {
     year () {
       return (new Date).getFullYear()
     }
   }
 }
 </script>
+<style>
+  .text-blue {
+    color: #2b80ff !important;
+  }
+  .text-bold {
+    font-weight: bold !important;
+  }
+</style>
