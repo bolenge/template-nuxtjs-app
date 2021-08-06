faf7b072fada34444d6b518ef8b0ba6371bf6618
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-08-06 22:22:05 +0100

    feat add content in home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,91 @@
 <template>
-  <Tutorial/>
+  <div class="content-wrapper">
+    <div class="row">
+      <div class="col-sm-12 mb-5">
+        <h3 class="mb-2 font-weight-bold">Bienvenue sur One Touch Application</h3>
+      </div>
+
+      <div class="col-xl-6 d-flex grid-margin stretch-card">
+        <div class="card">
+          <div class="card-body">
+            <div class="d-flex flex-wrap justify-content-between">
+              <h4 class="card-title mb-5">Aperçu des courriers</h4>
+            </div>
+            <div class="row">
+              <div class="col-12">
+                <div class="row">
+                  <div class="col-sm-12">
+                    <div class="d-flex justify-content-between mb-4">
+                      <div>Nombre de courriers reçus</div>
+                      <div class="text-muted">20</div>
+                    </div>
+                    <div class="d-flex justify-content-between mb-4">
+                      <div>Nombre de courriers envoyés</div>
+                      <div class="text-muted">23</div>
+                    </div>
+                    <div class="d-flex justify-content-between mb-4">
+                      <div>Nombre de nouveaux courriers</div>
+                      <div class="text-muted">12</div>
+                    </div>
+                    <div class="d-flex justify-content-between mb-4">
+                      <div>Nombre total de courriers</div>
+                      <div class="text-muted">168</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-xl-6 d-flex grid-margin stretch-card">
+        <div class="card">
+          <div class="card-body">
+            <div class="d-flex flex-wrap justify-content-between">
+              <h4 class="card-title mb-5">Aperçu des transactions financières</h4>
+            </div>
+            <div class="row">
+              <div class="col-12">
+                <div class="row">
+                  <div class="col-sm-12">
+                    <div class="d-flex justify-content-between mb-4">
+                      <div>Nombre total de requêtes de fonds</div>
+                      <div class="text-muted">20</div>
+                    </div>
+                    <div class="d-flex justify-content-between mb-4">
+                      <div>Nombre de requêtes conformes</div>
+                      <div class="text-muted">23</div>
+                    </div>
+                    <div class="d-flex justify-content-between mb-4">
+                      <div>Nombre de requêtes non conformes</div>
+                      <div class="text-muted">23</div>
+                    </div>
+                    <div class="d-flex justify-content-between mb-4">
+                      <div>Nombre de requêtes approuvées</div>
+                      <div class="text-muted">12</div>
+                    </div>
+                    <div class="d-flex justify-content-between mb-4">
+                      <div>Nombre de requêtes rejetées</div>
+                      <div class="text-muted">168</div>
+                    </div>
+                    <div class="d-flex justify-content-between mb-4">
+                      <div>Nombre de requêtes exécutées</div>
+                      <div class="text-muted">168</div>
+                    </div>
+                    <div class="d-flex justify-content-between mb-4">
+                      <div>Nombre total de requêtes</div>
+                      <div class="text-muted">168</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
