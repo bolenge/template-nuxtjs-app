5cf6a7a45729cf508f726e7989fbcf1082e7266c
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-08-08 21:11:18 +0100

    feat add chart in home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,85 +1,25 @@
 <template>
   <div class="content-wrapper">
     <div class="row">
-      <div class="col-sm-12 mb-5">
-        <h3 class="mb-2 font-weight-bold">Bienvenue sur One Touch Application</h3>
-      </div>
-
-      <div class="col-xl-6 d-flex grid-margin stretch-card">
+      <div class="col-lg-6 grid-margin grid-margin-lg-0 stretch-card">
         <div class="card">
           <div class="card-body">
-            <div class="d-flex flex-wrap justify-content-between">
-              <h4 class="card-title mb-5">Aperçu des courriers</h4>
-            </div>
-            <div class="row">
-              <div class="col-12">
-                <div class="row">
-                  <div class="col-sm-12">
-                    <div class="d-flex justify-content-between mb-4">
-                      <div>Nombre de courriers reçus</div>
-                      <div class="text-muted">20</div>
-                    </div>
-                    <div class="d-flex justify-content-between mb-4">
-                      <div>Nombre de courriers envoyés</div>
-                      <div class="text-muted">23</div>
-                    </div>
-                    <div class="d-flex justify-content-between mb-4">
-                      <div>Nombre de nouveaux courriers</div>
-                      <div class="text-muted">12</div>
-                    </div>
-                    <div class="d-flex justify-content-between mb-4">
-                      <div>Nombre total de courriers</div>
-                      <div class="text-muted">168</div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+            <h4 class="card-title">Courriers</h4>
+
+            <div class="container p-5">
+              <canvas id="pieChart"></canvas>
             </div>
           </div>
         </div>
       </div>
 
-      <div class="col-xl-6 d-flex grid-margin stretch-card">
+      <div class="col-lg-6 grid-margin grid-margin-lg-0 stretch-card">
         <div class="card">
           <div class="card-body">
-            <div class="d-flex flex-wrap justify-content-between">
-              <h4 class="card-title mb-5">Aperçu des transactions financières</h4>
-            </div>
-            <div class="row">
-              <div class="col-12">
-                <div class="row">
-                  <div class="col-sm-12">
-                    <div class="d-flex justify-content-between mb-4">
-                      <div>Nombre total de requêtes de fonds</div>
-                      <div class="text-muted">20</div>
-                    </div>
-                    <div class="d-flex justify-content-between mb-4">
-                      <div>Nombre de requêtes conformes</div>
-                      <div class="text-muted">23</div>
-                    </div>
-                    <div class="d-flex justify-content-between mb-4">
-                      <div>Nombre de requêtes non conformes</div>
-                      <div class="text-muted">23</div>
-                    </div>
-                    <div class="d-flex justify-content-between mb-4">
-                      <div>Nombre de requêtes approuvées</div>
-                      <div class="text-muted">12</div>
-                    </div>
-                    <div class="d-flex justify-content-between mb-4">
-                      <div>Nombre de requêtes rejetées</div>
-                      <div class="text-muted">168</div>
-                    </div>
-                    <div class="d-flex justify-content-between mb-4">
-                      <div>Nombre de requêtes exécutées</div>
-                      <div class="text-muted">168</div>
-                    </div>
-                    <div class="d-flex justify-content-between mb-4">
-                      <div>Nombre total de requêtes</div>
-                      <div class="text-muted">168</div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+            <h4 class="card-title">Requêtes de fonds</h4>
+
+            <div class="container p-5">
+              <canvas id="doughnutChart"></canvas>
             </div>
           </div>
         </div>
@@ -89,5 +29,98 @@
 </template>
 
 <script>
-export default {}
+import $ from 'jquery'
+import Chart from 'chart.js/auto';
+
+export default {
+  mounted () {
+  
+    var doughnutPieOptions = {
+      responsive: true,
+      animation: {
+        animateScale: true,
+        animateRotate: true
+      }
+    };
+
+    if ($("#doughnutChart").length) {
+      var dataRequestsFund = {
+        datasets: [{
+          data: [30, 40, 30, 25, 10, 5],
+          backgroundColor: [
+            'rgba(54, 162, 235, 0.5)',
+            'rgba(255, 206, 86, 0.5)',
+            'rgba(153, 102, 255, 0.5)',
+            'rgba(75, 192, 192, 0.5)',
+            'rgba(255, 99, 132, 0.5)',
+            'rgba(149, 241, 149, 0.5)',
+          ],
+          borderColor: [
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(255,99,132,1)',
+            'rgba(149, 241, 149, 1)'
+          ],
+        }],
+
+        // These labels appear in the legend and in the tooltips when hovering different arcs
+        labels: [
+          'Initiées : 30',
+          'Conformes : 40',
+          'Non conformes : 30',
+          'Approuvées : 25',
+          'Rejetées : 10',
+          'Exécutées : 5'
+        ]
+      };
+
+      var doughnutChartCanvas = $("#doughnutChart").get(0).getContext("2d");
+      new Chart(doughnutChartCanvas, {
+        type: 'doughnut',
+        data: dataRequestsFund,
+        options: doughnutPieOptions
+      });
+    }
+
+    if ($("#pieChart").length) {
+      var doughnutPieData = {
+        datasets: [{
+          data: [30, 40, 30],
+          backgroundColor: [
+            'rgba(54, 162, 235, 0.5)',
+            'rgba(255, 206, 86, 0.5)',
+            'rgba(75, 192, 192, 0.5)',
+            'rgba(153, 102, 255, 0.5)',
+            'rgba(255, 99, 132, 0.5)',
+            'rgba(255, 159, 64, 0.5)',
+          ],
+          borderColor: [
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255,99,132,1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+        }],
+
+        // These labels appear in the legend and in the tooltips when hovering different arcs
+        labels: [
+          'Reçus : 30',
+          'Envoyés : 40',
+          'Nouveaux : 30',
+        ]
+      };
+
+      var pieChartCanvas = $("#pieChart").get(0).getContext("2d");
+      new Chart(pieChartCanvas, {
+        type: 'pie',
+        data: doughnutPieData,
+        options: doughnutPieOptions
+      });
+    }
+  }
+}
 </script>
