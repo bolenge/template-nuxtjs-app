c41e66900fe0bc04c2ffe949e4a6c7c218690fb7
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-08-10 16:01:04 +0100

    fix link static files

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,16 +7,16 @@
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
             <title>Purple Admin</title>
             <!-- base:css -->
-            <link rel="stylesheet" href="vendors/typicons.font/font/typicons.css">
-            <link rel="stylesheet" href="vendors/css/vendor.bundle.base.css">
+            <link rel="stylesheet" href="/vendors/typicons.font/font/typicons.css">
+            <link rel="stylesheet" href="/vendors/css/vendor.bundle.base.css">
             <!-- endinject -->
             <!-- plugin css for this page -->
-            <link rel="stylesheet" href="vendors/select2/select2.min.css">
-            <link rel="stylesheet" href="vendors/select2-bootstrap-theme/select2-bootstrap.min.css">
-            <link rel="stylesheet" href="vendors/mdi/css/materialdesignicons.min.css" />
+            <link rel="stylesheet" href="/vendors/select2/select2.min.css">
+            <link rel="stylesheet" href="/vendors/select2-bootstrap-theme/select2-bootstrap.min.css">
+            <link rel="stylesheet" href="/vendors/mdi/css/materialdesignicons.min.css" />
             <!-- End plugin css for this page -->
             <!-- inject:css -->
-            <link rel="stylesheet" href="css/vertical-layout-light/style.css">
+            <link rel="stylesheet" href="/css/vertical-layout-light/style.css">
         </head>
         <body>
             <div class="container-scroller">
@@ -42,23 +42,23 @@
             </div>
             <!-- container-scroller -->
             <!-- base:js -->
-            <script src="vendors/js/vendor.bundle.base.js"></script>
+            <script src="/vendors/js/vendor.bundle.base.js"></script>
             <!-- endinject -->
             <!-- inject:js -->
-            <script src="js/off-canvas.js"></script>
-            <script src="js/hoverable-collapse.js"></script>
-            <script src="js/template.js"></script>
-            <script src="js/settings.js"></script>
-            <script src="js/todolist.js"></script>
+            <script src="/js/off-canvas.js"></script>
+            <script src="/js/hoverable-collapse.js"></script>
+            <script src="/js/template.js"></script>
+            <script src="/js/settings.js"></script>
+            <script src="/js/todolist.js"></script>
             <!-- endinject -->
             <!-- plugin js for this page -->
-            <script src="vendors/typeahead.js/typeahead.bundle.min.js"></script>
-            <script src="vendors/select2/select2.min.js"></script>
+            <script src="/vendors/typeahead.js/typeahead.bundle.min.js"></script>
+            <script src="/vendors/select2/select2.min.js"></script>
             <!-- End plugin js for this page -->
             <!-- Custom js for this page-->
-            <script src="js/file-upload.js"></script>
-            <script src="js/typeahead.js"></script>
-            <script src="js/select2.js"></script>
+            <script src="/js/file-upload.js"></script>
+            <script src="/js/typeahead.js"></script>
+            <script src="/js/select2.js"></script>
         </body>
     </html>
 </template>
