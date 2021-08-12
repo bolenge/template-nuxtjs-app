276af090d9e41eed93cf5341e64cb8445d46b14a
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-08-12 15:27:45 +0100

    feat add methods for user logged

--- mixins/Account.js
+++ mixins/Account.js
@@ -2,6 +2,17 @@ export default {
   computed: {
     userConnected() {
       return this.$auth.user
+    },
+    getUserMomentLogged() {
+      return this.formatDate('hh:mm', this.userConnected.track_user_login.date_logged)
+    },
+    currentAdmin() {
+      return this.$auth.user.admin
+    },
+    currentUserAvatar() {
+      return this.userConnected.avatar
     }
+  },
+  methods: {
   }
 }
