cd712611c89b9e82667603f8a7b86cbc72d488ec
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-08-14 15:18:58 +0100

    feat add tracker page not found

---
+++ pages/_.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  auth: false,
+  mounted() {
+    // Redirect all unexisting pages to 404 page
+    this.$router.push("/404");
+  },
+};
+</script>
