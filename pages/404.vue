8cb811bb06c22004b0686a7417d21161e3f58b71
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-08-14 15:19:07 +0100

    feat add 404 page

---
+++ pages/404.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="content-wrapper">
+    <div class="content-wrapper d-flex align-items-center text-center error-page">
+      <div class="row flex-grow">
+        <div class="col-lg-7 mx-auto py-5">
+          <div class="row align-items-center d-flex flex-row">
+            <div class="col-lg-6 text-lg-right pr-lg-4">
+              <h1 class="display-1 mb-0">404</h1>
+            </div>
+            <div class="col-lg-6 error-page-divider text-lg-left pl-lg-4">
+              <h2>SORRY!</h2>
+              <h3 class="font-weight-light">Cette page n'existe pas ou plus.</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
