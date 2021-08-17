455ae2d8d4b2008f590bf6e7a8c639e1f682b4a9
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-08-17 11:28:44 +0100

    feat add state for page and nav link actives

--- store/index.js
+++ store/index.js
@@ -1,5 +1,13 @@
 export const state = () => ({
+  page_active: 'home',
+  nav_link_active: ''
 })
 
 export const mutations = {
+  CHANGE_PAGE_ACTIVE(State, page) {
+    State.page_active = page
+  },
+  CHANGE_NAV_LINK_ACTIVE(State, navLink) {
+    State.nav_link_active = navLink
+  }
 }
