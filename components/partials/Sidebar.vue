e4a11c81fafc271a1188221f5324b5d646db3907
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-08-17 11:26:42 +0100

    feat add dynamic setter active items

--- components/partials/Sidebar.vue
+++ components/partials/Sidebar.vue
@@ -6,7 +6,10 @@
           </li>
 
           <!-- Item Dashboard -->
-          <li class="nav-item active">
+          <li
+            class="nav-item"
+            :class="setPageActive('home')"
+          >
             <nuxt-link class="nav-link" to="/">
               <em class="typcn typcn-device-desktop menu-icon"></em>
               <span class="menu-title">Accueil</span>
@@ -15,25 +18,63 @@
           <!-- End item Dashboard -->
 
           <!-- Item config. systeme -->
-          <li class="nav-item">
+          <li
+            class="nav-item"
+            :class="setPageActive('configs')"
+          >
             <a class="nav-link" data-toggle="collapse" href="#configs" aria-expanded="false" aria-controls="configs">
               <em class="typcn typcn-cog menu-icon"></em>
-              <span class="menu-title">Admin. système</span>
+              <span class="menu-title">Admin. Système</span>
               <em class="typcn typcn-chevron-right menu-arrow"></em>
             </a>
             <div class="collapse" id="configs">
               <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <nuxt-link class="nav-link" to="/configs/users">Config. utilisateurs</nuxt-link></li>
-                <li class="nav-item"> <nuxt-link class="nav-link" to="/configs/courriers">Config. courriers</nuxt-link></li>
-                <li class="nav-item"> <nuxt-link class="nav-link" to="/configs/finances">Config. finances</nuxt-link></li>
-                <li class="nav-item"> <nuxt-link class="nav-link" to="/configs/directions">Config. direction</nuxt-link></li>
+                <li class="nav-item">
+                  <nuxt-link
+                    to="/configs/users"
+                    class="nav-link"
+                    :class="setNavLinkActive('users-configs')"
+                  >
+                    Config. Utilisateurs
+                  </nuxt-link>
+                </li>
+                <li class="nav-item">
+                  <nuxt-link
+                    to="/configs/courriers"
+                    class="nav-link"
+                    :class="setNavLinkActive('courriers-configs')"
+                  >
+                    Config. Courriers
+                  </nuxt-link>
+                </li>
+                <li class="nav-item">
+                  <nuxt-link
+                    to="/configs/finances"
+                    class="nav-link"
+                    :class="setNavLinkActive('finances-configs')"
+                  >
+                    Config. Finances
+                  </nuxt-link>
+                </li>
+                <li class="nav-item">
+                  <nuxt-link
+                    to="/configs/directions"
+                    class="nav-link"
+                    :class="setNavLinkActive('directions-configs')"
+                  >
+                    Config. Direction
+                  </nuxt-link>
+                </li>
               </ul>
             </div>
           </li>
           <!-- End item config systeme -->
 
           <!-- Item Gestion courriers -->
-          <li class="nav-item">
+          <li
+            class="nav-item"
+            :class="setPageActive('courriers')"
+          >
             <a class="nav-link" data-toggle="collapse" href="#courriers" aria-expanded="false" aria-controls="courriers">
               <em class="typcn typcn-mail menu-icon"></em>
               <span class="menu-title">Gestion courriers</span>
@@ -41,16 +82,44 @@
             </a>
             <div class="collapse" id="courriers">
               <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <nuxt-link class="nav-link" to="/courriers/init">Initiation</nuxt-link></li>
-                <li class="nav-item"> <nuxt-link class="nav-link" to="/courriers/archives">Archives</nuxt-link></li>
-                <li class="nav-item"> <nuxt-link class="nav-link" to="/courriers/inbox">Boite de reception <span class="badge badge-primary p-1">4</span></nuxt-link></li>
+                <li class="nav-item">
+                  <nuxt-link
+                    to="/courriers/init"
+                    class="nav-link"
+                    :class="setNavLinkActive('init-courriers')"
+                  >
+                    Initiation
+                  </nuxt-link>
+                </li>
+                <li class="nav-item">
+                  <nuxt-link
+                    to="/courriers/archives"
+                    class="nav-link"
+                    :class="setNavLinkActive('archives-courriers')"
+                  >
+                    Archives
+                  </nuxt-link>
+                </li>
+                <li class="nav-item">
+                  <nuxt-link
+                    to="/courriers/inbox"
+                    class="nav-link"
+                    :class="setNavLinkActive('inbox-courriers')"
+                  >
+                    Boite de reception
+                    <span class="badge badge-primary p-1">4</span>
+                  </nuxt-link>
+                </li>
               </ul>
             </div>
           </li>
           <!-- End item Gestion courriers -->
 
           <!-- Item Requête de fonds -->
-          <li class="nav-item">
+          <li
+            class="nav-item"
+            :class="setPageActive('requests')"
+          >
             <a class="nav-link" data-toggle="collapse" href="#requests" aria-expanded="false" aria-controls="requests">
               <em class="typcn typcn-film menu-icon"></em>
               <span class="menu-title">Requête de fonds</span>
@@ -58,16 +127,43 @@
             </a>
             <div class="collapse" id="requests">
               <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <nuxt-link class="nav-link" to="/requests/init">CRF</nuxt-link></li>
-                <li class="nav-item"> <nuxt-link class="nav-link" to="/requests/inbox">Mes requêtes</nuxt-link></li>
-                <li class="nav-item"> <nuxt-link class="nav-link" to="/requests/synthesis">Synthese CRF</nuxt-link></li>
+                <li class="nav-item">
+                  <nuxt-link
+                    to="/requests/init"
+                    class="nav-link"
+                    :class="setNavLinkActive('init-requests')"
+                  >
+                    CRF
+                  </nuxt-link>
+                </li>
+                <li class="nav-item">
+                  <nuxt-link
+                    to="/requests/inbox"
+                    class="nav-link"
+                    :class="setNavLinkActive('inbox-requests')"
+                  >
+                    Mes requêtes
+                  </nuxt-link>
+                </li>
+                <li class="nav-item">
+                  <nuxt-link
+                    to="/requests/synthesis"
+                    class="nav-link"
+                    :class="setNavLinkActive('synthesis-requests')"
+                  >
+                    Synthese CRF
+                  </nuxt-link>
+                </li>
               </ul>
             </div>
           </li>
           <!-- End item Requête de fonds -->
 
           <!-- Item ressources financiers -->
-          <li class="nav-item">
+          <li
+            class="nav-item"
+            :class="setPageActive('finances')"
+          >
             <a class="nav-link" data-toggle="collapse" href="#finances" aria-expanded="false" aria-controls="finances">
               <em class="typcn typcn-credit-card menu-icon"></em>
               <span class="menu-title">Ressources financières</span>
@@ -75,16 +171,43 @@
             </a>
             <div class="collapse" id="finances">
               <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <nuxt-link class="nav-link" to="/finances/collection">Encaissement</nuxt-link></li>
-                <li class="nav-item"> <nuxt-link class="nav-link" to="/finances/transactions">Synthèse transactions</nuxt-link></li>
-                <li class="nav-item"> <nuxt-link class="nav-link" to="/finances/synthesis">Rapport financier</nuxt-link></li>
+                <li class="nav-item">
+                  <nuxt-link
+                    to="/finances/collection"
+                    class="nav-link"
+                    :class="setNavLinkActive('collection-finances')"
+                  >
+                    Encaissement
+                  </nuxt-link>
+                </li>
+                <li class="nav-item">
+                  <nuxt-link
+                    to="/finances/transactions"
+                    class="nav-link"
+                    :class="setNavLinkActive('transactions-finances')"
+                  >
+                    Synthèse Transactions
+                  </nuxt-link>
+                </li>
+                <li class="nav-item">
+                  <nuxt-link
+                    to="/finances/synthesis"
+                    class="nav-link"
+                    :class="setNavLinkActive('synthesis-finances')"
+                  >
+                    Rapport Financier
+                  </nuxt-link>
+                </li>
               </ul>
             </div>
           </li>
           <!-- End item ressources financieres -->
 
           <!-- Item Modifier mot de passe -->
-          <li class="nav-item">
+          <li
+            class="nav-item"
+            :class="setPageActive('change-password')"
+          >
             <nuxt-link class="nav-link" to="/account/change-password">
               <em class="typcn typcn-key menu-icon"></em>
               <span class="menu-title">Modifier mot de passe</span>
@@ -106,6 +229,23 @@
 
 <script>
 export default {
-  
+  props: {
+    pageActive: {
+      type: String,
+      default: 'home'
+    },
+    navLinkActive: {
+      type: String,
+      default: ''
+    },
+  },
+  methods: {
+    setPageActive(page) {
+      return this.pageActive == page ? 'active' : ''
+    },
+    setNavLinkActive(navLink) {
+      return this.navLinkActive == navLink ? 'active' : ''
+    },
+  }
 }
 </script>
