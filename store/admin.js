9fdf52b3db92f2e1d3cb9ccaba2bb82c87149081
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-08-19 12:48:52 +0100

    feat add store admin

---
+++ store/admin.js
@@ -0,0 +1,36 @@
+export const state = () => ({
+  admins: [],
+  loading: false
+})
+
+export const mutations = {
+  SET_ADMINS(State, payload) {
+    State.admins = payload
+  },
+  SET_LOADING(State, payload) {
+    State.loading = payload
+  }
+}
+
+export const actions = {
+  load({ commit }) {
+    commit('SET_LOADING', true)
+
+    this.$axios.get('admins').then(({ data }) => {
+      commit('SET_ADMINS', data.results)
+    }).finally((_) => {
+      commit('SET_LOADING', false)
+    })
+  },
+  show (_, payload) {
+    return new Promise((resolve, reject) => {
+      this.$axios.get(`admins/${payload.id}`)
+        .then(({ data }) => {
+          resolve(data.results)
+        })
+        .catch((error) => {
+          reject(error)
+        })
+    })
+  },
+}
