e2226807b9318e68bfee66d8dc190442bac9a6e6
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-08-19 07:58:43 +0100

    feat add loading state and show action in fonction store

--- store/fonction.js
+++ store/fonction.js
@@ -1,20 +1,36 @@
 export const state = () => ({
-  fonctions: []
+  fonctions: [],
+  loading: false
 })
 
 export const mutations = {
   SET_FONCTIONS(State, payload) {
     State.fonctions = payload
+  },
+  SET_LOADING(State, payload) {
+    State.loading = payload
   }
 }
 
 export const actions = {
-  load ({ commit }) {
+  load({ commit }) {
+    commit('SET_LOADING', true)
+
     this.$axios.get('fonctions').then(({ data }) => {
-      console.log('Data fonctions', data.results);
       commit('SET_FONCTIONS', data.results)
-    }).catch((error) => {
-      console.log(error)
+    }).finally((_) => {
+      commit('SET_LOADING', false)
     })
-  }
+  },
+  show (_, payload) {
+    return new Promise((resolve, reject) => {
+      this.$axios.get(`fonctions/${payload.id}`)
+        .then(({ data }) => {
+          resolve(data.results)
+        })
+        .catch((error) => {
+          reject(error)
+        })
+    })
+  },
 }
