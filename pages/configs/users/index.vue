f482d4dc0e64a61a3020b300cccd3f8ee6cebc25
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-08-19 12:48:40 +0100

    fix refactor index config users

--- pages/configs/users/index.vue
+++ pages/configs/users/index.vue
@@ -5,60 +5,18 @@
     </div>
     <div class="row">
       <div class="col-lg-12 grid-margin stretch-card">
-        <div class="card">
-          <div class="card-body">
-            <nuxt-link class="btn btn-info btn-sm float-right" to="/configs/users/create">&plus; Ajouter un utilisateur</nuxt-link>
-            <h4 class="card-title text-normal">Liste des utilisateurs</h4>
-            <div class="table-responsive">
-              <table class="table table-striped" aria-describedby="List users">
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>Avatar</th>
-                    <th>Nom</th>
-                    <th>Prenom</th>
-                    <th>Sexe</th>
-                    <th>Email</th>
-                    <th>Telephone</th>
-                    <th>Fonction</th>
-                    <th>Departement</th>
-                    <th>Actions</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr v-for="i in 8" :key="i">
-                    <td>{{ i }}</td>
-                    <td class="py-1">
-                      <img src="/images/faces/face1.jpg" alt="image"/>
-                    </td>
-                    <td>Don de Dieu</td>
-                    <td>Bolenge</td>
-                    <td>M</td>
-                    <td>dondedieubolenge@gmail.com</td>
-                    <td>0909876543</td>
-                    <td>Developer</td>
-                    <td>Developpement</td>
-                    <td>
-                      <button class="btn btn-sm btn-info">
-                        <span class="typcn typcn-pencil"></span>
-                      </button>
-                      <button class="btn btn-sm btn-danger">
-                        <span class="typcn typcn-trash"></span>
-                      </button>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
+        <TableFilter
+          :headers="headers"
+          model="admin"
+          :buttonCreate="buttonCreate"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Form from '@/components/crud/Form'
+import TableFilter from '@/components/crud/TableFilter'
 import Global from '~/mixins/Global'
 export default {
   middleware: 'auth',
@@ -68,12 +26,78 @@ export default {
     }
   },
   mixins: [Global],
+  components: {
+    TableFilter
+  },
+  data() {
+    return {
+      headers: [
+        {
+          text: 'Avatar',
+          value: 'avatar',
+          type: 'image',
+          baseUrl: 'storage/user_pictures'
+        },
+        {
+          text: 'Nom',
+          value: 'lastname',
+          type: 'string'
+        },
+        {
+          text: 'Prénom',
+          value: 'firstname',
+          type: 'string'
+        },
+        {
+          text: 'Sexe',
+          value: 'gender',
+          type: 'string'
+        },
+        {
+          text: 'Email',
+          value: 'email',
+          type: 'string'
+        },
+        {
+          text: 'Role',
+          value: 'user.role.name',
+          type: 'object'
+        },
+        {
+          text: 'Téléphone',
+          value: 'phone',
+          type: 'string'
+        },
+        {
+          text: 'Fonction',
+          value: 'fonction.name',
+          type: 'object'
+        },
+        {
+          text: 'Departement',
+          value: 'department.name',
+          type: 'object'
+        },
+        {
+          text: 'Actions',
+          value: '',
+          type: 'actions'
+        }
+      ]
+    }
+  },
   computed: {
     currentPage() {
       return 'configs'
     },
     currentNavLink() {
       return 'users-configs'
+    },
+    buttonCreate() {
+      return {
+        link: '/configs/users/create',
+        text: 'Ajouter un utilisateur'
+      }
     }
   },
   methods: {
