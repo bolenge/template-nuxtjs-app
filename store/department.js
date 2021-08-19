3fe10519a6b187b9a13c0a0a5c5962076c455ceb
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-08-19 07:58:28 +0100

    feat add loading state and show action

--- store/department.js
+++ store/department.js
@@ -1,20 +1,36 @@
 export const state = () => ({
-  departments: []
+  departments: [],
+  loading: false
 })
 
 export const mutations = {
   SET_DEPARTMENTS(State, payload) {
     State.departments = payload
+  },
+  SET_LOADING(State, payload) {
+    State.loading = payload
   }
 }
 
 export const actions = {
-  load ({ commit }) {
+  load({ commit }) {
+    commit('SET_LOADING', true)
+
     this.$axios.get('departments').then(({ data }) => {
-      console.log('Data departments', data.results);
       commit('SET_DEPARTMENTS', data.results)
-    }).catch((error) => {
-      console.log(error)
+    }).finally((_) => {
+      commit('SET_LOADING', false)
     })
-  }
+  },
+  show (_, payload) {
+    return new Promise((resolve, reject) => {
+      this.$axios.get(`departments/${payload.id}`)
+        .then(({ data }) => {
+          resolve(data.results)
+        })
+        .catch((error) => {
+          reject(error)
+        })
+    })
+  },
 }
