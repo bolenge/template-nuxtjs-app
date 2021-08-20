f32c9948c58142c7ba862400f5120fde7d574e2d
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-08-20 10:46:17 +0100

    feat add component config type courrier

---
+++ components/configs/courriers/ConfigTypeCourrier.vue
@@ -0,0 +1,83 @@
+<template>
+  <ConfigPanel
+    title="Configuration Courriers"
+    createFormTitle="Création de type courrier"
+    editFormTitle="Edition du type courrier"
+    model="type_courrier"
+    tableTitle="Liste de type courriers"
+    iconTitlte="typcn-mail"
+    :edited="edited"
+    :entityEdited="entityEdited"  
+    :entity="entity"
+    :headers="headers"
+    :fields="fields"
+    @launchEdited="onLaunchEdit"
+    @edited="onEdited"
+    @entityReseted="onEntityReseted"
+  />
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+import ConfigPanel from '../ConfigPanel'
+
+export default {
+  components: {
+    ConfigPanel
+  },
+  data() {
+    return {
+      fields: [
+        {
+          name: 'name',
+          type: 'text',
+          required: true,
+          label: 'Libellé'
+        },
+        {
+          name: 'code',
+          type: 'text',
+          required: true,
+          label: 'Code'
+        }
+      ],
+      entity: {},
+      headers: [
+        {
+          text: 'Libellé',
+          value: 'name',
+          type: 'string'
+        },
+        {
+          text: 'Code',
+          value: 'code',
+          type: 'string'
+        },
+        {
+          text: 'Actions',
+          value: '',
+          type: 'actions'
+        }
+      ],
+      edited: false,
+      entityEdited: null
+    }
+  },
+  methods: {
+    ...mapActions({
+      showTypeCourrier: 'type_courrier/show'
+    }),
+    async onLaunchEdit(id) {
+      this.entityEdited = await this.showTypeCourrier({id})
+      this.edited = true
+    },
+    onEdited() {
+      this.entityEdited = null
+      this.edited = false
+    },
+    onEntityReseted() {
+      this.entity = {}
+    }
+  }
+}
+</script>
