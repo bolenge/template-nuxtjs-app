739d152d7dc149a8ef9feb435970306d7cb464f3
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-08-20 14:19:04 +0100

    feat add refactor  inde config finances

--- pages/configs/finances/index.vue
+++ pages/configs/finances/index.vue
@@ -7,11 +7,11 @@
             <a class="nav-link active" id="config-banks-tab" data-toggle="tab" href="#config-banks" role="tab" aria-controls="config-banks" aria-selected="true"><span class="typcn typcn-credit-card"></span> Comptes Bancaires</a>
           </li>
           <li class="nav-item" role="presentation">
-            <a class="nav-link" id="config-caisses-tab" data-toggle="tab" href="#config-caisses" role="tab" aria-controls="config-caisses" aria-selected="false"><sapn class="typcn typcn-document-text"></sapn> Comptes Petites Caisses</a>
+            <a class="nav-link" id="config-caisses-tab" data-toggle="tab" href="#config-caisses" role="tab" aria-controls="config-caisses" aria-selected="false"><span class="typcn typcn-document-text"></span> Comptes Caisses</a>
           </li>
           <!-- Tab config rubriques -->
           <li class="nav-item" role="presentation">
-            <a class="nav-link" id="config-tags-tab" data-toggle="tab" href="#config-tags" role="tab" aria-controls="config-tags" aria-selected="false"><sapn class="typcn typcn-tags"></sapn> Rubriques</a>
+            <a class="nav-link" id="config-tags-tab" data-toggle="tab" href="#config-tags" role="tab" aria-controls="config-tags" aria-selected="false"><span class="typcn typcn-tags"></span> Nature Opérations</a>
           </li>
           <!-- End Tab config rubriques -->
         </ul>
@@ -20,78 +20,12 @@
 
     <div class="tab-content" id="myTabContent">
       <div class="tab-pane fade show active" id="config-banks" role="tabpanel" aria-labelledby="config-banks-tab">
-        <h2 class="title mb-4"><span class="typcn typcn-credit-card"></span> Configuration Comptes Bancaires</h2>
-
-        <div class="row">
-          <div class="col-md-4 grid-margin stretch-card">
-            <div class="card">
-              <div class="card-body">
-                <h4 class="card-title text-normal">Creation Compte</h4>
-
-                <form class="forms-sample">
-                  <div class="form-group">
-                    <label for="bank">Nom de la banque <span class="text-danger">*</span></label>
-                    <input type="text" class="form-control form-control-sm" id="bank" required />
-                  </div>
-
-                  <div class="form-group">
-                    <label for="account_number">Nº Compte <span class="text-danger">*</span></label>
-                    <input type="text" class="form-control form-control-sm" id="account_number" required />
-                  </div>
-
-                  <div class="form-group">
-                    <label for="account_number">Nom proprietaire compte <span class="text-danger">*</span></label>
-                    <input type="text" class="form-control form-control-sm" id="account_number" required />
-                  </div>
-                  <button type="submit" class="btn btn-info mr-2">Enregistrer</button>
-                </form>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-8 grid-margin stretch-card">
-            <div class="card">
-              <div class="card-body">
-                <h4 class="card-title text-normal">Liste de departements</h4>
-
-                <div class="table-responsive">
-                  <table class="table table-hover" aria-describedby="">
-                    <thead>
-                      <tr>
-                        <th>#</th>
-                        <th>Banque</th>
-                        <th>Nº Compte</th>
-                        <th>Nom proprietaire compte</th>
-                        <th>Actions</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr v-for="i in 2" :key="i">
-                        <td>{{ i }}</td>
-                        <td>Equity</td>
-                        <td>3847-8377-0393-3847</td>
-                        <td>Mabiala Matondo</td>
-                        <td>
-                          <button class="btn btn-sm btn-info">
-                            <span class="typcn typcn-pencil"></span>
-                          </button>
-                          <button class="btn btn-sm btn-danger">
-                            <span class="typcn typcn-trash"></span>
-                          </button>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <ConfigAccountBank />
       </div>
 
-      <!-- Tab config petites caisses -->
+      <!-- Tab config caisses -->
       <div class="tab-pane fade" id="config-caisses" role="tabpanel" aria-labelledby="config-caisses-tab">
-        <h1 class="title mb-4"><sapn class="typcn typcn-document-text"></sapn> Configuration Petites Caisses</h1>
+        <h1 class="title mb-4"><span class="typcn typcn-document-text"></span> Configuration Caisses</h1>
 
         <div class="row">
           <div class="col-md-4 grid-margin stretch-card">
@@ -153,12 +87,12 @@
           </div>
         </div>
       </div>
-      <!-- End Tab config petites caisses -->
+      <!-- End Tab config caisses -->
 
       <!-- Tab config Rubriquess -->
       <div class="tab-pane fade" id="config-tags" role="tabpanel" aria-labelledby="config-tags-tab">
         <h1 class="title mb-4">
-          <sapn class="typcn typcn-tags"></sapn> Configuration Rubriques
+          <span class="typcn typcn-tags"></span> Configuration Rubriques
         </h1>
 
         <div class="row">
@@ -220,6 +154,7 @@
 <script>
 
 import Global from '~/mixins/Global'
+import ConfigAccountBank from '~/components/configs/finances/ConfigAccountBank'
 export default {
   middleware: 'auth',
   head() {
@@ -228,6 +163,9 @@ export default {
     }
   },
   mixins: [Global],
+  components: {
+    ConfigAccountBank
+  },
   computed: {
     currentPage() {
       return 'configs'
