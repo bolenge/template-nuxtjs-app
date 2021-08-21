e2fad5af82c2398b1b73a5045847fb6ae8cc431a
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-08-21 08:12:12 +0100

    feat add store nature

---
+++ store/nature.js
@@ -0,0 +1,36 @@
+export const state = () => ({
+  natures: [],
+  loading: false
+})
+
+export const mutations = {
+  SET_NATURES(State, payload) {
+    State.natures = payload
+  },
+  SET_LOADING(State, payload) {
+    State.loading = payload
+  }
+}
+
+export const actions = {
+  load({ commit }) {
+    commit('SET_LOADING', true)
+
+    this.$axios.get('natures').then(({ data }) => {
+      commit('SET_NATURES', data.results)
+    }).finally((_) => {
+      commit('SET_LOADING', false)
+    })
+  },
+  show (_, payload) {
+    return new Promise((resolve, reject) => {
+      this.$axios.get(`natures/${payload.id}`)
+        .then(({ data }) => {
+          resolve(data.results)
+        })
+        .catch((error) => {
+          reject(error)
+        })
+    })
+  },
+}
