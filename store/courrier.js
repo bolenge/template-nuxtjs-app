c5d805a98936b5cc013b5a4699b1730692158f9c
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-08-22 03:40:29 +0100

    feat add loadCourriersNoTransmitted and transmits actions

--- store/courrier.js
+++ store/courrier.js
@@ -33,4 +33,47 @@ export const actions = {
         })
     })
   },
+  loadCourriersNoTransmitted({ commit }) {
+    commit('SET_LOADING', true)
+
+    this.$axios.get('courriers/all/transmitted').then(({ data }) => {
+      commit('SET_COURRIERS', data.results)
+    }).finally((_) => {
+      commit('SET_LOADING', false)
+    })
+  },
+  transmits({ dispatch, commit }, payload) {
+    commit('SET_LOADING', true)
+    
+    return new Promise((resolve, reject) => {
+      this.$axios.post(`courriers/transmits/${payload.id}`)
+        .then(({ data }) => {
+          if (data.state) {
+            this.$swal({
+              title: "Transmission",
+              text: 'Operation effectuée avec succès',
+              icon: "success",
+              confirmButtonText: 'Ok'
+            })
+          }
+
+          dispatch(`courrier/load`, {}, { root: true })
+          dispatch(`courrier/loadCourriersNoTransmitted`, {}, { root: true })
+
+          resolve(data.results)
+        })
+        .catch((error) => {
+          this.$swal({
+            title: "Erreur",
+            text: 'Une erreur est survenue lors de la transmission, réessayez svp !',
+            icon: "warning",
+            confirmButtonText: 'Ok'
+          })
+
+          reject(error)
+        }).finally((_) => {
+          commit('SET_LOADING', false)
+        })
+    })
+  },
 }
