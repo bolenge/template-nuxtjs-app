5d60cd731236ef7d6884177a0551f6075f802bc7
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-08-22 01:06:37 +0100

    feat add dynamisation init courrier

--- pages/courriers/init.vue
+++ pages/courriers/init.vue
@@ -6,74 +6,17 @@
 
     <div class="row">
       <div class="col-lg-8 grid-margin stretch-card">
-        <div class="card">
-          <div class="card-body">
-            <form class="forms-sample">
-              <div class="row">
-              <!-- Field type -->
-                <div class="col-lg-6">
-                  <div class="form-group">
-                    <label for="type">Type de courrier <span class="text-danger">*</span></label>
-                    <select class="form-control" id="type" required style="height: 39px;">
-                      <option value="M">Entrant</option>
-                      <option value="F">Sortant</option>
-                    </select>
-                  </div>
-                </div>
-                <!-- End Field type -->
-
-                <!-- Field code -->
-                <div class="col-lg-6">
-                  <div class="form-group">
-                    <label for="code">Code courrier (<small>Généré automatiquement</small>) <span class="text-danger">*</span></label>
-                    <input type="text" class="form-control form-control-sm" id="code" required/>
-                  </div>
-                </div>
-                <!-- End Field code -->
-
-                <!-- Field sender -->
-                <div class="col-lg-6">
-                  <div class="form-group">
-                    <label for="sender">Expediteur <span class="text-danger">*</span></label>
-                    <input type="text" class="form-control form-control-sm" id="sender" required/>
-                  </div>
-                </div>
-                <!-- End Field sender -->
-
-                <!-- Field receiver -->
-                <div class="col-lg-6">
-                  <div class="form-group">
-                    <label for="receiver">Destinataire <span class="text-danger">*</span></label>
-                    <select class="form-control" id="receiver" required style="height: 39px;">
-                      <option value="M">Homme</option>
-                      <option value="F">Femme</option>
-                    </select>
-                  </div>
-                </div>
-                <!-- End Field receiver -->
-
-                <!-- Field file scanned -->
-                <div class="col-lg-12">
-                  <div class="form-group">
-                      <label>Uploader le fichier scanner ici</label>
-                      <input type="file" name="img[]" class="file-upload-default">
-                      <div class="input-group col-xs-12">
-                        <input type="text" class="form-control file-upload-info" disabled placeholder="Upload fichier">
-                        <span class="input-group-append">
-                          <button class="file-upload-browse btn btn-light" type="button">Choisir le fichier</button>
-                        </span>
-                      </div>
-                    </div>
-                </div>
-                <!-- End Field file scanned -->
-              </div>
-              
-              <div class="text-center">
-                <button type="submit" class="btn btn-info"><span class="typcn typcn-input-checked"></span> Enregistrer</button>
-              </div>
-            </form>
-          </div>
-        </div>
+        <!-- Form create courriers -->
+        <Create
+          api="courriers"
+          model="courrier"
+          title="Enregistrement d'un nouveau courrier"
+          :fields="fields"
+          :entity="entity"
+          :formRow="true"
+          @submitted="onSubmit"
+        />
+        <!-- End Form create courriers -->
       </div>
     </div>
 
@@ -81,7 +24,10 @@
   </div>
 </template>
 <script>
+import { mapState, mapActions } from 'vuex'
 import Global from '~/mixins/Global'
+import Account from '~/mixins/Account'
+import Create from '~/components/crud/Create'
 
 export default {
   middleware: 'auth',
@@ -90,14 +36,95 @@ export default {
       title: 'Initiation d\'un courrier'
     }
   },
-  mixins: [Global],
+  mixins: [Global, Account],
+  components: {
+    Create
+  },
+  data() {
+    return {
+      fields: [
+        {
+          name: 'type_courrier_id',
+          type: 'select',
+          required: true,
+          label: 'Type de courrier',
+          items: this.typeCourriers,
+          syncField: 'code',
+          syncCapterValue: 'code',
+          syncFieldFromValue: 'code'
+        },
+        {
+          name: 'code',
+          type: 'text',
+          required: true,
+          label: 'Code courrier (Généré automatiquement)',
+          value: this.formatDate('hh/mm/ss/dd/MM/yy', new Date),
+          isSync: true
+        },
+        {
+          name: 'sender',
+          type: 'text',
+          required: true,
+          label: 'Expediteur '
+        },
+        {
+          name: 'admin_recipient_id',
+          type: 'select',
+          required: true,
+          label: 'Destinataire',
+          items: this.recipients,
+          itemText: 'firstname',
+        },
+        {
+          name: 'attachment',
+          type: 'file',
+          required: true,
+          label: 'Uploader le fichier scanner ici',
+        },
+      ],
+      entity: {}
+    }
+  },
   computed: {
+    ...mapState({
+      typeCourriers(state) {
+        return state.type_courrier.type_courriers
+      },
+      recipients(state) {
+        return state.admin.admins
+      },
+    }),
     currentPage() {
       return 'courriers'
     },
     currentNavLink() {
       return 'init-courriers'
     },
+    adminId() {
+      return this.currentAdmin.id
+    }
+  },
+  watch: {
+    typeCourriers() {
+      this.$set(this.fields[0], 'items', this.typeCourriers)
+    },
+    recipients() {
+      this.$set(this.fields[3], 'items', this.recipients)
+    },
+  },
+  methods: {
+    ...mapActions({
+      loadTypeCourriers: 'type_courrier/load',
+      loadRecipients: 'admin/load',
+    }),
+    onSubmit(entity) {
+      this.entity = {}
+      this.$router.replace('/courriers/archives')
+    },
+  },
+  mounted() {
+    this.loadTypeCourriers()
+    this.loadRecipients()
   }
 }
 </script>
