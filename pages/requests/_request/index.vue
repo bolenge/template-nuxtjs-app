88c74c76b1e5199df7b1080449fbee3ef9ccb2e3
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-08-23 16:11:29 +0100

    feat type acount

---
+++ pages/requests/_request/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <Edit
+    :slug="slug"
+  />
+</template>
+
+<script>
+import Edit from '~/components/requests/Edit'
+import Global from '~/mixins/Global'
+
+export default {
+  middleware: 'auth',
+  head() {
+    return {
+      title: 'Detail d\'une requete de fonds'
+    }
+  },
+  mixins: [Global],
+  components: {
+    Edit
+  },
+  asyncData ({ params }) {
+    const slug = +params.request
+    return { slug }
+  },
+}
+</script>
