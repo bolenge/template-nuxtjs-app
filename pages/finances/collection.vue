28a5f5c2b78d4088fde8b76b3ccbbd7ebc5ab9f0
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-08-24 20:26:04 +0100

    fix refactor collection

--- pages/finances/collection.vue
+++ pages/finances/collection.vue
@@ -1,90 +1,11 @@
 <template>
   <div class="content-wrapper">
-    <div class="container-fluid">
-      <h2 class="title mb-4"><span class="typcn typcn-credit-card"></span> Encaissement de Fonds</h2>
-    </div>
-
-    <div class="row">
-      <div class="col-lg-8 grid-margin stretch-card">
-        <div class="card">
-          <div class="card-body">
-            <form class="forms-sample">
-              <div class="row">
-                <!-- Field source -->
-                <div class="col-lg-6">
-                  <div class="form-group">
-                    <label for="source">Source <span class="text-danger">*</span></label>
-                    <input type="text" class="form-control form-control-sm" id="source" required/>
-                  </div>
-                </div>
-                <!-- End Field source -->
-
-              <!-- Field currency -->
-                <div class="col-lg-6">
-                  <div class="form-group">
-                    <label for="currency">Devise <span class="text-danger">*</span></label>
-                    <select class="form-control" id="currency" required style="height: 39px;">
-                      <option value="M">USD</option>
-                      <option value="F">CDF</option>
-                    </select>
-                  </div>
-                </div>
-                <!-- End Field currency -->
-
-                <!-- Field amount -->
-                <div class="col-lg-6">
-                  <div class="form-group">
-                    <label for="amount">Montant <span class="text-danger">*</span></label>
-                    <input type="number" class="form-control form-control-sm" id="amount" required/>
-                  </div>
-                </div>
-                <!-- End Field amount -->
-
-                <!-- Field type_account -->
-                <div class="col-lg-6">
-                  <div class="form-group">
-                    <label for="type_account">Type compte à approvionner <span class="text-danger">*</span></label>
-                    <select
-                      v-model="form.type_account"
-                      class="form-control"
-                      id="type_account"
-                      equired
-                      style="height: 39px;"
-                    >
-                      <option value="1">Compte Bancaire</option>
-                      <option value="2">Petites Caisses</option>
-                    </select>
-                  </div>
-                </div>
-                <!-- End Field type_account -->
-
-                <!-- Field receiver -->
-                <div v-if="form.type_account == '1'" class="col-lg-6">
-                  <div class="form-group">
-                    <label for="receiver">Compte approvionné <span class="text-danger">*</span></label>
-                    <select class="form-control" id="receiver" required style="height: 39px;">
-                      <option value="M">Rawbank - 8765-8387-9903-9876</option>
-                      <option value="M">Rawbank - 8765-8387-9903-9876</option>
-                    </select>
-                  </div>
-                </div>
-                <!-- End Field receiver -->
-              </div>
-              
-              <div class="text-center">
-                <button type="submit" class="btn btn-info"><span class="typcn typcn-input-checked"></span> Enregistrer</button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <script src="/js/file-upload.js"></script>
+    <Create />
   </div>
 </template>
 <script>
 import Global from '~/mixins/Global'
+import Create from '~/components/finances/Create'
 
 export default {
   middleware: ['auth', 'auth-superadmin-admin'],
@@ -94,6 +15,9 @@ export default {
     }
   },
   mixins: [Global],
+  components: {
+    Create
+  },
   data() {
     return {
       form: {
