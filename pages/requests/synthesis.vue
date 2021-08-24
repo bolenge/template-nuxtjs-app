1bc7af0ac01ae97396f9cf4a978cbf90256a34a8
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-08-24 02:22:33 +0100

    feat load inbox fund requests

--- pages/requests/synthesis.vue
+++ pages/requests/synthesis.vue
@@ -31,7 +31,7 @@ export default {
     }
   },
   mixins: [Global],
-   components: {
+  components: {
     TableFilter
   },
   data() {
--- store/fund_request.js
+++ store/fund_request.js
@@ -1,5 +1,6 @@
 export const state = () => ({
   fund_requests: [],
+  inbox_fund_requests: [],
   loading: false
 })
 
@@ -9,7 +10,10 @@ export const mutations = {
   },
   SET_LOADING(State, payload) {
     State.loading = payload
-  }
+  },
+  SET_INBOX_FUND_REQUESTS(State, payload) {
+    State.inbox_fund_requests = payload
+  },
 }
 
 export const actions = {
@@ -22,6 +26,7 @@ export const actions = {
       commit('SET_LOADING', false)
     })
   },
+
   show (_, payload) {
     return new Promise((resolve, reject) => {
       this.$axios.get(`fund_requests/${payload.id}`)
@@ -31,6 +36,17 @@ export const actions = {
         .catch((error) => {
           reject(error)
         })
+    })
+  },
+
+  loadInboxFundRequestsAdmin({ commit }) {
+    commit('SET_LOADING', true)
+
+    this.$axios.get('fund_requests/all/inbox').then(({ data }) => {
+      commit('SET_FUND_REQUESTS', data.results)
+      commit('SET_INBOX_FUND_REQUESTS', data.results)
+    }).finally((_) => {
+      commit('SET_LOADING', false)
     })
   },
 }
