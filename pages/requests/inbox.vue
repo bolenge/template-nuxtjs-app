d2c6e345be5d0866dc987b0366e28e46db1c33d4
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-08-24 02:22:05 +0100

    fix refactor inbox fund requests

--- pages/requests/inbox.vue
+++ pages/requests/inbox.vue
@@ -5,95 +5,16 @@
     </div>
     <div class="row">
       <div class="col-lg-12 grid-margin stretch-card">
-        <div class="card">
-          <div class="card-body">
-            <div class="d-flex">
-              <div class="col-lg-8">
-                <nuxt-link to="/requests/init" class="btn btn-info btn-sm mr-lg-3">
-                  <span class="typcn typcn-plus"></span> Initiation
-                </nuxt-link>
-
-                <button class="btn btn-success btn-sm">
-                  <span class="typcn typcn-refresh"></span> Actualiser
-                </button>
-
-                <button class="btn btn-info btn-sm mx-lg-3">
-                  <span class="typcn typcn-database"></span> Extraction données
-                </button>
-
-                <button class="btn btn-light btn-sm">
-                  <span class="typcn typcn-chevron-left"></span>
-                </button>
-                <span>1/10</span>
-                <button class="btn btn-light btn-sm">
-                  <span class="typcn typcn-chevron-right"></span>
-                </button>
-              </div>
-              <div class="form-group col-4">
-                <div class="input-group">
-                  <label for="search" class="mr-3 mt-2">Filtre : </label>
-                  <input type="search" id="search" class="form-control form-control-sm py-2" placeholder="Recherche..." aria-label="Search">
-                  <div class="input-group-append">
-                    <button class="btn btn-sm btn-light" type="button"><span class="typcn typcn-zoom"></span></button>
-                  </div>
-                </div>
-              </div>
-            </div>
-            
-            <div class="table-responsive">
-              <table class="table table-hover" aria-describedby="">
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>Date</th>
-                    <th>Objet</th>
-                    <th>Monant</th>
-                    <th>Devise</th>
-                    <th>Rubrique</th>
-                    <th>Nature OP.</th>
-                    <th>Comptes</th>
-                    <th>Status</th>
-                    <th>Actions</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr
-                    v-for="i in 10"
-                    :key="i"
-                  >
-                    <td>{{ i }}</td>
-                    <td>{{ formatDate('dd/MM/yyyy', new Date) }}</td>
-                    <td>Lorem ipsum dolor sit amet consectetur...</td>
-                    <td>250</td>
-                    <td>USD</td>
-                    <td>Voyage</td>
-                    <td>---</td>
-                    <td>---</td>
-                    <td>
-                      <label
-                        class="badge"
-                        :class="{'badge-success': numberIsPair(randValue), 'badge-primary': !numberIsPair(randValue)}"
-                      >
-                          {{ !numberIsPair(randValue) ? 'Rejete' : 'Approuve' }}
-                        </label>
-                    </td>
-                    <td>
-                      <button class="btn btn-sm btn-info">
-                        <span class="typcn typcn-pencil"></span>
-                      </button>
-                      <button class="btn btn-sm btn-danger">
-                        <span class="typcn typcn-trash"></span>
-                      </button>
-                      <button class="btn btn-sm btn-success">
-                        <span class="typcn typcn-eye-outline"></span>
-                      </button>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
+        <TableFilter
+          :headers="headers"
+          model="fund_request"
+          actionLoad="loadInboxFundRequestsAdmin"
+          :buttonCreate="buttonCreate"
+          :buttons="buttonsAction"
+          :extractData="true"
+          @launchEdited="onLaunchEdited"
+          @showed="onShowed"
+        />
       </div>
     </div>
   </div>
@@ -101,6 +22,7 @@
 
 <script>
 import Global from '~/mixins/Global'
+import TableFilter from '~/components/crud/TableFilter'
 
 export default {
   middleware: 'auth',
@@ -110,9 +32,80 @@ export default {
     }
   },
   mixins: [Global],
+  components: {
+    TableFilter
+  },
   data() {
     return {
-      j: 1
+      headers: [
+        {
+          text: 'Date',
+          value: 'created_at',
+          type: 'date',
+          filterable: true
+        },
+        {
+          text: 'Initiateur',
+          value: 'admin_fund_requestor.user.name',
+          type: 'object',
+          filterable: true
+        },
+        {
+          text: 'Objet',
+          value: 'object',
+          type: 'string',
+          filterable: true
+        },
+        {
+          text: 'Montant',
+          value: 'amount',
+          type: 'string',
+          filterable: true
+        },
+        {
+          text: 'Devise',
+          value: 'currency.code',
+          type: 'object',
+          filterable: true
+        },
+        {
+          text: 'Taux',
+          value: 'rate',
+          type: 'amount-money',
+          filterable: true
+        },
+        {
+          text: 'Montant en USD',
+          value: 'calcul',
+          type: 'amount-money',
+          filterable: true,
+        },
+        {
+          text: 'Status',
+          value: 'statuts',
+          type: 'badge',
+          types: {
+            'Conforme': 'badge-success',
+            'Non conforme': 'badge-danger',
+            'Approuvé': 'badge-success',
+            'Executé': 'badge-success',
+            'Rejeté': 'badge-danger',
+            'En etude': 'badge-warning',
+          },
+          filterable: true
+        },
+        {
+          text: 'Actions',
+          value: '',
+          type: 'actions',
+          filterable: false
+        }
+      ],
+      buttonsAction: {
+        edit: false,
+        delete: false,
+        show: true
+      }
     }
   },
   computed: {
@@ -125,6 +118,20 @@ export default {
     currentNavLink() {
       return 'inbox-requests'
     },
+    buttonCreate() {
+      return {
+        link: '/requests/init',
+        text: 'CRF'
+      }
+    }
+  },
+  methods: {
+    onLaunchEdited(id) {
+      // Do something
+    },
+    onShowed(id) {
+      this.$router.replace('/requests/'+id)
+    }
   }
 }
 </script>
