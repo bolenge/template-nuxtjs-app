49c7c384c4a9ae4cb571282b619db86ed5b2bcee
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-08-25 08:59:32 +0100

    feat add action loadCollectionTransactions

--- store/transaction.js
+++ store/transaction.js
@@ -1,11 +1,15 @@
 export const state = () => ({
   transactions: [],
+  collection_transactions: [],
   loading: false
 })  
 
 export const mutations = {
   SET_TRANSACTIONS(State, payload) {
     State.transactions = payload
+  },
+   SET_COLLECTION_TRANSACTIONS(State, payload) {
+    State.collection_transactions = payload
   },
   SET_LOADING(State, payload) {
     State.loading = payload
@@ -31,6 +35,17 @@ export const actions = {
         .catch((error) => {
           reject(error)
         })
+    })
+  },
+
+  loadCollectionTransactions({ commit }) {
+    commit('SET_LOADING', true)
+
+    this.$axios.get('transactions/all/collection').then(({ data }) => {
+      console.log('data.results', data.results);
+      commit('SET_COLLECTION_TRANSACTIONS', data.results)
+    }).finally((_) => {
+      commit('SET_LOADING', false)
     })
   },
 }
