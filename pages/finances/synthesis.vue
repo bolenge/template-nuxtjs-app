3a20881a180903a2c2a8bac9af425c221bf7c614
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-08-25 08:05:49 +0100

    fix refactor finances synthesis

--- pages/finances/synthesis.vue
+++ pages/finances/synthesis.vue
@@ -5,68 +5,10 @@
     </div>
     <div class="row">
       <div class="col-lg-12 grid-margin stretch-card">
-        <div class="card">
-          <div class="card-body">
-            <div class="d-flex">
-              <div class="col">
-                <button class="btn btn-success btn-sm mr-3">
-                  <span class="typcn typcn-refresh"></span> Actualiser
-                </button>
-
-                <button class="btn btn-info btn-sm ml-3">
-                  <span class="typcn typcn-database"></span> Extraction données
-                </button>
-              </div>
-              <div class="form-group col">
-                <div class="input-group">
-                  <label for="search" class="mr-3 mt-2">Filtre : </label>
-                  <input type="text" id="search" class="form-control form-control-sm py-2" placeholder="Recherche..." aria-label="Search">
-                  <div class="input-group-append">
-                    <button class="btn btn-sm btn-light" type="button"><span class="typcn typcn-zoom"></span></button>
-                  </div>
-                </div>
-              </div>
-            </div>
-            
-            
-
-            <div class="table-responsive">
-              <table class="table table-hover" aria-describedby="">
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>Type opération</th>
-                    <th>Op. en attente</th>
-                    <th>Op. declinées</th>
-                    <th>Op. approuvées</th>
-                    <th>Nombre de personnes</th>
-                    <th>Montant</th>
-                    <th>Actions</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr
-                    v-for="i in 2"
-                    :key="i"
-                  >
-                    <td>{{ i }}</td>
-                    <td>{{ i === 1 ? 'Décaissement' : 'Encaissement' }}</td>
-                    <td>20</td>
-                    <td>13</td>
-                    <td>6</td>
-                    <td>49</td>
-                    <td>49 000 USD</td>
-                    <td>
-                      <button class="btn btn-sm btn-info">
-                        <span class="typcn typcn-eye-outline"></span>
-                      </button>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
+        <RapportAccountBank />
+      </div>
+      <div class="col-lg-12 grid-margin stretch-card">
+        <RapportAccountCashs />
       </div>
     </div>
   </div>
@@ -74,6 +16,8 @@
 
 <script>
 import Global from '~/mixins/Global'
+import RapportAccountBank from '~/components/finances/RapportAccountBank'
+import RapportAccountCashs from '~/components/finances/RapportAccountCashs'
 
 export default {
   middleware: ['auth', 'auth-superadmin-admin'],
@@ -83,6 +27,10 @@ export default {
     }
   },
   mixins: [Global],
+  components: {
+    RapportAccountBank,
+    RapportAccountCashs,
+  },
   data() {
     return {
       j: 1
