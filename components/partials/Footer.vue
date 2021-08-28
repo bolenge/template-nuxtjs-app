6575919bced5240f48d1da07bc5d18d97afb8937
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-08-28 13:58:48 +0100

    fix rename One by Neo

--- components/partials/Footer.vue
+++ components/partials/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="footer">
     <div class="d-sm-flex justify-content-center justify-content-sm-between">
-      <span class="text-center text-sm-left d-block d-sm-inline-block">Copyright © <a href="#" target="_blank" class="text-blue text-bold">One Touch</a> {{ year }} </span>
+      <span class="text-center text-sm-left d-block d-sm-inline-block">Copyright © <a href="#" target="_blank" class="text-blue text-bold">Neo Touch</a> {{ year }} </span>
       <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Powered by <a href="https://mtechinteractives.com/" target="_blank" class="text-blue text-bold">Mtech Interactives</a></span>
     </div>
   </footer>
