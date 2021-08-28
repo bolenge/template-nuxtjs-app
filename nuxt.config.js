c2a38a9151d7eabeea0910e0484b8d5c48344d7d
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-08-28 14:44:42 +0100

    feat add font Montserrat

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,6 +27,10 @@ export default {
     ],
     link: [
       {
+        rel: 'stylesheet',
+        href: '"https://fonts.googleapis.com/css?family=Montserrat:400,500,700"'
+      },
+      {
         rel: "stylesheet",
         href: "/vendors/typicons.font/font/typicons.css",
       },
