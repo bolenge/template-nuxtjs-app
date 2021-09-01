38728c47ade1bee4e9020dad95a95ba09a59ca5e
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-09-01 04:46:38 +0100

    feat add dynamisation forgot password

--- pages/forgot-password/index.vue
+++ pages/forgot-password/index.vue
@@ -6,16 +6,26 @@
           <div class="col-lg-4 mx-auto">
             <div class="auth-form-light text-left py-5 px-4 px-sm-5">
 
-              <h4>Mot de passe oublié</h4>
-              <h6 class="font-weight-light">Saisissez votre email pour réinitialiser votre mot de passe</h6>
+             <div class="text-center">
+                <h4>Mot de passe oublié</h4>
+                <h6 class="font-weight-light">Saisissez votre email pour réinitialiser votre mot de passe</h6>
+             </div>
 
-              <form class="pt-3">
+              <form class="pt-3" @submit.prevent="onSubmit">
                 <div class="form-group">
-                  <input type="email" class="form-control" placeholder="Email" />
+                  <input
+                    v-model="form.email"
+                    type="email"
+                    class="form-control form-control-sm font-weight-300 font-size-13px"
+                    placeholder="Email"
+                  />
                 </div>
 
-                <div class="mt-3">
-                  <button class="btn btn-block btn-info btn-lg font-weight-medium auth-form-btn">ENVOYER</button>
+                <div class="mt-3 text-center">
+                  <button class="btn btn-info btn-sm py-2 px-3">
+                    <span v-if="loading">Chargement...</span>
+                    <span v-else>ENVOYER</span>
+                  </button>
                 </div>
                 <div class="text-center mt-4 font-weight-light">
                   <nuxt-link to="/login"><em class="typcn typcn-arrow-left"></em> Je veux me connecter.</nuxt-link>
@@ -24,7 +34,7 @@
             </div>
 
             <div class="text-center py-5">
-              <p class="font-size-20px">Powered by <strong><a href="https://mtechinteractives.com">Mtech Interactives</a></strong></p>
+              <p class="font-size-20px font-weight-300">Powered by <strong><a href="https://mtechinteractives.com">Mtech Interactives</a></strong></p>
             </div>
           </div>
         </div>
@@ -37,6 +47,7 @@
 </template>
 
 <script>
+import { mapActions } from "vuex";
 export default {
   watch: {
   },
@@ -44,6 +55,56 @@ export default {
   head() {
     return {
       title: 'Mot de passe oublié'
+    }
+  },
+  data () {
+    return {
+      form: {},
+      loading: false
+    }
+  },
+  methods: {
+    ...mapActions({
+      forgotPassword: 'user/forgotPassword'
+    }),
+    async onSubmit() {
+      this.loading = true
+
+      try {
+        await this.forgotPassword({ entity: this.form })
+
+        this.$swal({
+          title: "Opération effectuée avec succès.",
+          text: "Un mail vous est envoyé pour réinitialiser votre mot de passe.",
+          icon: "success",
+          buttons: true,
+          confirmButtonText: 'Ok'
+        }).then(({isConfirmed}) => {
+          this.$auth.logout()
+        });
+
+      } catch (error) {
+        if (error.response && (error.response.status === 422 || error.response.status === 401)) {
+          if (error.response.data) {
+            const data = error.response.data
+            
+            this.$swal({
+              title: "Erreur",
+              text: data.message,
+              icon: "warning",
+              buttons: true,
+              confirmButtonText: 'Ok'
+            })
+          }else {
+            this.$toast.error('Une erreur est survenue, réessayez svp !')
+          }
+        }else {
+          this.$toast.error('Une erreur est survenue, réessayez svp !')
+        }
+        
+      } finally {
+        this.loading = false
+      }
     }
   }
 }
