2bf84bd5f3395fe9a4fcc56465c68d40d6b904dd
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-09-06 15:17:16 +0100

    feat add print page content

---
+++ pages/requests/print/_request/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <PrintCRF
+    :slug="slug"
+    backLink="/requests/synthesis"
+    title="Mes Requêtes"
+  />
+</template>
+
+<script>
+import PrintCRF from '~/components/requests/PrintCRF'
+import Global from '~/mixins/Global'
+
+export default {
+  layout: 'login',
+  middleware: 'auth',
+  head() {
+    return {
+      title: 'Detail d\'un CRF'
+    }
+  },
+  mixins: [Global],
+  components: {
+    PrintCRF
+  },
+  asyncData ({ params }) {
+    const slug = +params.request
+    return { slug }
+  },
+  computed: {
+    currentPage() {
+      return 'requests'
+    },
+    currentNavLink() {
+      return 'synthesis-requests'
+    },
+  }
+}
+</script>
