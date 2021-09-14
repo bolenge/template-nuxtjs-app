4072d6a67a4548e586a7b59c2ba056664d8a7805
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-09-14 08:22:33 +0100

    feat add config category nature in config rubrics

--- pages/configs/rubrics/index.vue
+++ pages/configs/rubrics/index.vue
@@ -3,21 +3,27 @@
     <div class="mb-4">
       <div class="col-lg-12 d-flex justify-content-center">
         <ul class="nav nav-tabs" id="myTab" role="tablist">
+          <!-- Tab config category-natures -->
+          <li class="nav-item" role="presentation">
+            <a class="nav-link py-2 active" id="config-category-natures-tags-tab" data-toggle="tab" href="#config-category-natures" role="tab" aria-controls="config-category-natures" aria-selected="false"><span class="typcn typcn-tags"></span> Nature Op. (Niveau 1)</a>
+          </li>
+          <!-- End Tab config category-natures -->
+
           <!-- Tab config natures -->
           <li class="nav-item" role="presentation">
-            <a class="nav-link py-2 active" id="config-tags-tab" data-toggle="tab" href="#config-natures" role="tab" aria-controls="config-natures" aria-selected="false"><span class="typcn typcn-tags"></span> Nature Op. (Niveau 1)</a>
+            <a class="nav-link py-2" id="config-natures-tags-tab" data-toggle="tab" href="#config-natures" role="tab" aria-controls="config-natures" aria-selected="false"><span class="typcn typcn-tags"></span> Nature Op. (Niveau 2)</a>
           </li>
           <!-- End Tab config natures -->
 
           <!-- Tab config sub-natures -->
           <li class="nav-item" role="presentation">
-            <a class="nav-link py-2" id="config-tags-tab" data-toggle="tab" href="#config-sub-natures" role="tab" aria-controls="config-sub-natures" aria-selected="false"><span class="typcn typcn-tags"></span> Nature Op. (Niveau 2)</a>
+            <a class="nav-link py-2" id="config-sub-natures-tags-tab" data-toggle="tab" href="#config-sub-natures" role="tab" aria-controls="config-sub-natures" aria-selected="false"><span class="typcn typcn-tags"></span> Nature Op. (Niveau 3)</a>
           </li>
           <!-- End Tab config sub-natures -->
 
           <!-- Tab config comptes-natures -->
           <li class="nav-item" role="presentation">
-            <a class="nav-link py-2" id="config-tags-tab" data-toggle="tab" href="#config-comptes-natures" role="tab" aria-controls="config-comptes-natures" aria-selected="false"><span class="typcn typcn-tags"></span> Comptes Op. (Niveau 3)</a>
+            <a class="nav-link py-2" id="config-comptes-natures-tags-tab" data-toggle="tab" href="#config-comptes-natures" role="tab" aria-controls="config-comptes-natures" aria-selected="false"><span class="typcn typcn-tags"></span> Comptes Op. (Niveau 4)</a>
           </li>
           <!-- End Tab config comptes-natures -->
         </ul>
@@ -25,17 +31,11 @@
     </div>
 
     <div class="tab-content" id="myTabContent">
-      <!-- Tab pane config comptes bancaires -->
-      <div class="tab-pane fade show active" id="config-banks" role="tabpanel" aria-labelledby="config-banks-tab">
-        <ConfigAccountBank />
+      <!-- Tab config Nature d'Operations (Niveau 1) -->
+      <div class="tab-pane fade show active" id="config-category-natures" role="tabpanel" aria-labelledby="config-category-natures-tab">
+        <ConfigCategoryNature />
       </div>
-      <!--  End tab pane config comptes bancaires -->
-
-      <!-- Tab config comptes caisses -->
-      <div class="tab-pane fade" id="config-caisses" role="tabpanel" aria-labelledby="config-caisses-tab">
-        <ConfigAccountCash />
-      </div>
-      <!-- End Tab config comptes caisses -->
+      <!-- Tab config Nature d'Operations (Niveau 1) -->
 
       <!-- Tab config Nature d'Operations (Niveau 1) -->
       <div class="tab-pane fade" id="config-natures" role="tabpanel" aria-labelledby="config-natures-tab">
@@ -61,11 +61,10 @@
 <script>
 
 import Global from '~/mixins/Global'
-import ConfigAccountBank from '~/components/configs/finances/ConfigAccountBank'
-import ConfigAccountCash from '~/components/configs/finances/ConfigAccountCash'
 import ConfigNature from '~/components/configs/finances/ConfigNature'
 import ConfigSubNature from '~/components/configs/finances/ConfigSubNature'
 import ConfigCompteNature from '~/components/configs/finances/ConfigCompteNature'
+import ConfigCategoryNature from '~/components/configs/finances/ConfigCategoryNature'
 export default {
   middleware: 'auth',
   head() {
@@ -75,11 +74,10 @@ export default {
   },
   mixins: [Global],
   components: {
-    ConfigAccountBank,
-    ConfigAccountCash,
     ConfigNature,
     ConfigSubNature,
     ConfigCompteNature,
+    ConfigCategoryNature,
   },
   computed: {
     currentPage() {
